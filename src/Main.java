ea7b845b10629a69552941ac75742bd3db5afea4
Anton Olsson <antonolsson.imp@gmail.com>
2022-11-02 17:00:26 +0100

    Hello

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         CoffeeMachine coffeeMachine = new CoffeeMachine();
         coffeeMachine.start();
+        System.out.println();
     }
 }
